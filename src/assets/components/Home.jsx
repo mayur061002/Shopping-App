cf1f09cb0afe6da862566d5a82f429418643e269
mayur061002 <mayurmakode131@gmail.com>
2024-09-04 18:18:29 +0530

    products adding using api success

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from './Nav'
 import { Link } from 'react-router-dom'
+import { ProductContext } from '../utils/Context'
+import Loading from './Loading'
 
 const Home = () => {
     
-  return (
+  const [products] = useContext(ProductContext);
+  console.log(products)
+
+  return products ? ( 
+
     <>
     <Nav />
         <div className='w-[85%] h-screen p-5 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto  '>
 
-            <Link to="/details/1"  className='card p-3 border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col hover:scale-110 mr-3 mb-3'>
-              <div className='w-full h-[80%] bg-contain bg-no-repeat bg-center  '
-                style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}>
+            {products.map((p, i) => 
 
-              </div>
+              <Link 
+                key={p.id}
+                to={`/details/${p.id}`} 
+                className='card p-3 border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col hover:scale-110 mr-3 mb-3'>
+                <div className='w-full h-[80%] bg-contain bg-no-repeat bg-center  '
+                  style={{backgroundImage: `url(${p.image})`}}>
 
-              <h1 className='hover:text-blue-400 mt-[10px] '>Lorem ipsum dolor amet.</h1>
-            </Link>
+                </div>
+
+                <h1 className='hover:text-blue-400 mt-[10px] '>{p.title}</h1>
+              </Link>
+
+            )}
+
 
         </div>
     </>  
-  )
-}
+  ) : (
+    <Loading />
+  );
+};
 
 export default Home
